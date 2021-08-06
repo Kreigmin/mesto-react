3a5c46633e3fc15ad3d81690252be33b7eadef46
Dmitry Sobolev <leejjuk@gmail.com>
2021-08-06 16:59:23 +0300

    refactor: delete file format  for Api

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ import AddPlacePopup from "./AddPlacePopup.js";
 import DeleteCardPopup from "./DeleteCardPopup.js";
 import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
 
-import api from "../utils/Api.js";
+import api from "../utils/Api";
 
 function App() {
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
